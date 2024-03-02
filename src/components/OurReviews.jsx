ccc9961fc975efc62a35bd5aa04c1a0c28720703
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2024-03-03 00:56:44 +0530

    done with the testimonials part

--- src/components/OurReviews.jsx
+++ src/components/OurReviews.jsx
@@ -1,8 +1,21 @@
 "use client"
 
 import React from 'react'
+import testimonials from '../json/data'
+import Image from 'next/image'
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const OurReviews = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className='main w-[100%] h-[100vh] p-[80px]'>
       <div className='top flex gap-[90px] flex-row justify-start items-start'>
@@ -12,7 +25,33 @@ const OurReviews = () => {
         <h1 className='font-semibold text-[28px] max-w-[300px] text-[#232323]'>Feedback from our favorite customers</h1>
       </div>
       <div>
-        <div className='testimonial'>
+        <div className='testimonial pt-[150px]'>
+      <Slider {...settings}>
+          {
+            testimonials?.map((data, index) => (
+              <div className='test_main  gap-[120px] text-black'>
+                <div className='rounded-full '>
+                  <Image
+                  src="./Logo.svg"
+                  width={120}
+                  height={120}
+                  alt='profile_img'
+                  className='rounded-full border w-[120px] h-[120px] border-b-2 border-black'
+                  />
+                </div>
+                <div className='flex flex-col '>
+                  <p className='text inline'>
+                <span className="font-bold">{data.index.split('/')[0]}</span><span>{'/'}</span>
+    <span>{data.index.split('/')[1]}</span>
+                  </p>
+                  <p className='max-w-[700px] text-[18px] italic font-normal leading-[38.4px] text-[#232323]'>"{data.testimonials}"</p>
+                  <p className='pt-4 font-bold text-[#000] text-[23px]'>{data.name} {data.surname}</p>
+                  <p className='text-[#000] text-[18px] font-normal'>{data.post} </p>
+                </div>
+              </div>
+            ))
+          }
+        </Slider>
         </div>
       </div>
       </div>
--- src/json/data.js
+++ src/json/data.js
@@ -1,6 +1,6 @@
 const testimonials = [
     {
-      "index": "01",
+      "index": "01/03",
       "image": "path/to/your/image1.jpg",
       "testimonials": "Alice is an exceptional engineer who consistently delivers high-quality code on time. Her problem-solving skills are unmatched.Working with Alice has been a pleasure. She's not only technically proficient but also a great team player, always willing to help and share her knowledge.Alice's dedication to her work is truly inspiring. She goes above and beyond to ensure that projects are successful and stakeholders are satisfied.",
       "name": "Alice",
@@ -9,7 +9,7 @@ const testimonials = [
       
     },
     {
-      "index": "02",
+      "index": "02/03",
       "image": "path/to/your/image1.jpg",
       "testimonials": "Alice is an exceptional engineer who consistently delivers high-quality code on time. Her problem-solving skills are unmatched.Working with Alice has been a pleasure. She's not only technically proficient but also a great team player, always willing to help and share her knowledge.Alice's dedication to her work is truly inspiring. She goes above and beyond to ensure that projects are successful and stakeholders are satisfied.",
       "name": "Alice",
@@ -17,7 +17,7 @@ const testimonials = [
       "post": "Senior Software Engineer"
     },
     {
-      "index": "03",
+      "index": "03/03",
       "image": "path/to/your/image1.jpg",
       "testimonials": "Alice is an exceptional engineer who consistently delivers high-quality code on time. Her problem-solving skills are unmatched.Working with Alice has been a pleasure. She's not only technically proficient but also a great team player, always willing to help and share her knowledge.Alice's dedication to her work is truly inspiring. She goes above and beyond to ensure that projects are successful and stakeholders are satisfied.",
       "name": "Alice",
